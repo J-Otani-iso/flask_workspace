9a1e2a409ddfb18da2a7eabebccea7289bfefe04
junya <otani.junya@isoroot.jp>
2026-07-31 18:57:44 +0900

    finish p130

--- ch3/use_database/app.py
+++ ch3/use_database/app.py
@@ -15,4 +15,48 @@ db = SQLAlchemy(app)
 
 # メッセージのデータベースモデル
 class Message(db.Model):
-    id = db.Column(db.Interger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(100))
+    contents = db.Column(db.String(100))
+
+@app.route("/")
+def index():
+    search_word: str = request.args.get("search_word")
+
+    if search_word is None:
+        # search_wordパラメータが存在しない場合は、すべてのメッセージを「top.html」に表示
+        message_list: list[Message] = Message.query.all()
+    else:
+        # search_wordパラメータが存在する場合は、検索ワードでフィルター↓メッセージを「top.html」に表示
+        message_list: list[Message] = Message.query.filter(Message.contents.like(f"%{search_word}")).all()
+
+    return render_template(
+        "top.html",
+        login_user_name = login_user_name,
+        message_list=message_list,
+        search_word=search_word,
+    )
+
+@app.route("/write", methods=["GET", "POST"])
+def write():
+    if request.method == "GET":
+        return render_template("write.html", login_user_name=login_user_name)
+
+    elif request.method == "POST":
+        # POSTメソッドのフォームの値を利用して、新しいメッセージを作成
+        contents: str = request.form.get("contents")
+        user_name: str = request.form.get("user_name")
+        new_message = Message(user_name=user_name, contents=contents)
+        db.session.add(new_message)
+        # 変更をデータベースにコミット
+        db.session.commit()
+
+        # 「/」にリダイレクト
+        return redirect(url_for("index"))
+    
+# データベースの初期化
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
