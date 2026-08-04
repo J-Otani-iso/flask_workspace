bb9a25e35c0a9560be9410168f2ad3768673cc6c
junya2632 <21rd038@ms.dendai.ac.jp>
2026-08-04 23:55:16 +0900

    finish p163

---
+++ ch3/use_nosql/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+# Socket.ioのセットアップ - (*1)
+socketio = SocketIO(app)
+
+# MongoDBの接続先設定 - (*2)
+mongo_uri = "mongodb+srv://otanijunya_db_user:jQk9KM57dyoAmMh6@cluster0.jgo9x46.mongodb.net/?appName=Cluster0"
+client = MongoClient(mongo_uri)
+db = client["SNS"]
+messages_collection = db["messages"]
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# メッセージの読み込み - (*3)
+@socketio.on('load messages')
+def load_messages():
+    messages = messages_collection.find().sort('_id', -1).limit(10)
+    messages = list(messages)[::-1]
+    messages_return = [message['message'] for message in messages]
+    # メッセージをクライアントへ送信 - (*4)
+    emit('load all messages', messages_return)
+
+# メッセージの登録 - (*5)
+@socketio.on('send message')
+def send_message(message):
+    messages_collection.insert_one({'message': message})
+    # メッセージをクライアントへ送信 - (*6)
+    emit('load one message', message, broadcast=True)
+
+
+if __name__ == "__main__":
+    # Socketioサーバの起動 - (*7)
+    socketio.run(app, debug=True)
